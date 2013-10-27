fc00e328b78b315becc8da5842d708ecdec3caf5
Santiago <santiagocastillo79@gmail.com>
2013-10-27 03:01:27 -0500

    Initial commit.

---
+++ cuentos/cuentos.js
@@ -0,0 +1,50 @@
+// Used to avoid polluting the namespace.
+var Morrocoi = Morrocoi || {};
+Morrocoi.cuentos = {};
+
+Morrocoi.cuentos.list = [
+	{
+		"tittle": "Tonos rosas",
+		"date": "10/27/2013",
+		"author": "Nathaly B.",
+		"body": "Era una noche llena de caras nuevas, los conocía desde hacía poco pero los sentía parte de mí desde hacía mucho tiempo. Una lucha por vestidos, medias y zapatos, los tonos rosas se mezclaban en cada paso de mi búsqueda, quería evitarlos pero parecían ser la solución adecuada. Vestidos color rosa uno al lado del otro, parecía que me decían que ellos eran los elegidos para esta noche tan especial."
+			+ "<br /><br />Con estos rostros nuevos pero que inspiraban seguridad, amor y confianza, seguí mi búsqueda."
+			+ "<br /><br />Era una búsqueda más grande que de vestidos medias y zapatos, todas estábamos buscando algo más, eso que se nos había perdido hacía mucho tiempo y no lográbamos encontrar. Sabíamos que lo habíamos perdido, peor no estábamos seguras de donde o cuando, simplemente nos hacía falta, no estábamos completas."
+			+ "<br /><br />En medio de todo el caos, el trancón y las bolsas llenas de nuestros hermosos vestidos, logramos a través todos esos obstáculos que nos retrasaban en nuestra búsqueda. Llegamos era un lugar conocido, pero aún nos esperaban muchas sorpresas por descubrir de este lugar que se había vuelto ese pequeño resiento de felicidad."
+			+ "<br /><br />No éramos las únicas luchando por llegar a tiempo con todos nuestros implementos, no sabíamos que nos esperaba, solo veíamos todas esas caras de incertidumbre y alegría, pintando cuerpos de verde, enmascarando brazos de superhéroes."
+			+ "<br /><br />Entro la calma a todos, después se abrieron esas puertas de madera entramos en orden emocionados. Todo era una alegría total. En medio de esta alegría fuimos en búsqueda de aquello que se nos había perdido."
+			+ "<br /><br />Estaba hay en ese pequeño cuarto, de colores rosa, emocionada, ansiosa. Me sudaban las manos, no podía dejar de sonreír, me habían dicho que había alguien afuera esperándome, ¿Quién podría ser? Todos esos pensamientos cruzaron mi cabeza uno tras otro. Estaba atrapada en cada uno de ellos."
+			+ "<br /><br />Cerré los ojos, camine confiando ciegamente, espere que fuera hora de abrir mis ojos y ver esta persona sorpresa que me estaba esperando, por esa por la cual me vestí de colores rosa."
+			+ "<br /><br />En ese momento en que abrí los ojos, sentía muchas cosas, miedo, expectativa, ansiedad, pero nunca pensé que en ese momento encontraría eso que se me había perdido desde hace tanto tiempo. Era lo más hermoso y mágico, era ese vestido que nunca debe dejar tirado, era una joya preciosa que encajaba perfectamente en mi pecho, ese vestido perfecto que me hacía lucir increíble."
+			+ "<br /><br />Ese día, apenas abrí mis ojos vi un brillo segador que cubría todo el espacio, solo existíamos nosotras dos. Ese día prometí nunca perder ese algo otra vez, ese día me puse ese vestido hermoso y esa joya maravillosa. Desde ese día soy yo otra vez, la dueña de esta vida maravillosa que vive intensamente."
+			+ "<br /><br />Veo a mi alrededor, esta oscuro, la lluvia cae por la ventana, tic tac tic tac, una y otra vez. Me levanto, siento que algo extraordinario ha pasado. Era un sueño maravilloso, donde yo era una princesa que bailaba felizmente, viendo su reflejo. Me levanto de la cama, está haciendo frio, camino por la oscuridad de mi cuarto, veo algo cerca al espejo, es un gran brillo que proviene de mí, no era un sueño era un maravilloso recuerdo."
+	},
+	{
+		"tittle": "Obsesión",
+		"date": "10/27/2013",
+		"author": "Nathaly B.",
+		"body": "En una preciosa cárcel adornada de pies a cabeza vivía ella, quien todos los días al mirar ese espejo reluciente junto a la ventana, no podía ver sino esa cara que encerraba su verdadero ser."
+			+ "<br /><br />Ese apartamento igual de arreglado que su cárcel, era el único testigo de su vida, él era el único que la conocía, que la había visto tal y como era. Con él no guardaba ningún secreto, él se había vuelto su único confidente, el único que la consolaba y la ayudaba a refugiarse de eso a lo que ella más le temía, a la soledad y la verdad."
+			+ "<br /><br />De noche pensaba que era lo que faltaba, que era lo que la tenia encerrada, esta cárcel que había construido con el paso de los años, ahora se volvía su mayor tormento, ahora era presa de su propio invento."
+			+ "<br /><br />Esta cárcel se había construido siguiendo las instrucciones de su prisionera, era una cárcel grande, imponente, llena de color y aparente alegría. Siempre bien aseada y muy bien presentada, adornada con los más grandes adornos que resaltaban aun mas su belleza."
+			+ "<br /><br />Esta cárcel construida para derrumbar los muros de las inseguridades, poco a poco se fue convirtiendo en un muro aun más grande, en el muro de las apariencias."
+			+ "<br /><br />Este muro no conocía sentimientos, ni razones, no concedía una lagrima o una sonrisa. A este no le pasan los años, ni el tiempo, este no aprende, no perdona. Este muro no permitía equivocaciones, engaños, ni reproches. Así poco a poco, ella se fue convirtiendo en su propia cárcel."
+			+ "<br /><br />Cuando ella era joven se le escuchaba reír, se alegraba de estar viva y ser ella misma; se dice que le encantaban los chocolates, los helados y las malteadas, la bandeja paisa y demás manjares típicos. También se le vio muy feliz con un hombre a su lado, que más que darle amor, daba su vida por ella. Él la amaba tal y como era, así descomplicada y sencilla."
+			+ "<br /><br />Sus amigos de la infancia recuerdan con emoción su sencillez y sus sueños, en los que en primer lugar estaba formar una gran familia con muchos hijos, un hogar con mucho amor y permanecer cerca de la familia, porque ella tenía muy claro que eso era lo importante en la vida."
+			+ "<br /><br />Ahora, cuando el tiempo ha forjado muros tan altos como el cielo, que no dejan ver ni un solo rasgo de lo que ella algún día fue, ella se pregunta porque nadie puede verla de verdad, porque la soledad la rodea en su hermosa prisión."
+			+ "<br /><br />Como remedio para esa soledad en la que se encuentra, usa su cárcel para atraer bellos hombres que le den pequeños momentos de felicidad, antes solo había un hombre que la hacía inmensamente feliz, ahora en su prisión, eran varios los hombres que la tenían, que la llenaban de regalos, pero ninguno de ellos la conocía, ninguno de ellos sabía que el chocolate era el camino a su corazón, ellos solo sabían llenarla de joyas y vestidos, para así fortalecer aun más su prisión y encerrarla más."
+			+ "<br /><br />Se dice que todo este cambio aparecido, cuando esta mujer vio que la vida no se limitaba a la familia, ella esperaba más y salió a conseguirlo como muchas mujeres le habían dicho que lo lograría, haciendo de su cuerpo su cárcel."
+			+ "<br /><br />Desde ese momento olvido sonríe, llorar y gritar, dejando a tras la persona que era, olvidando el amor y la ternura, volviéndose un ser egocéntrico y egoísta. Así fue como poco a poco lo perdió a él a su único amor, a él que la vio llorar y reír, que la veía con sus sueños y locuras, tal y como ella era."
+			+ "<br /><br />En esa búsqueda de la perfección, perdió todo, se perdió ella misma encerrada en esa prisión que ella tanto quería. Esta gran construcción en busca del cuerpo perfecto, de la imagen perfecta, la dejo sola y vacía, sin sueños, sin metas y como ella temía sola."
+			+ "<br /><br />Después de forjar una gran coraza que la llevara a donde ella esperaba ir, llegar y ver lo vacio que es el mundo de las apariencias, llora lo perdido, llora su amor, llora su vida, que desperdicio en busca de un sueño que no existía."
+	}
+];
+
+Morrocoi.cuentos.parse = function(cuento){
+	return '<div class="story">'
+				+ '<span class="date">' + cuento.date + '</span>'
+				+	'<h4>' + cuento.tittle + '</h4>'
+				+	'<p>' + cuento.body + '</p>'
+				+	'<span class="signature">- ' + cuento.author + '</span>'
+				+ '</div>';
+};
